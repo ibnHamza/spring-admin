d4b9202e4a4fee59a9f163217194a0a502ffc5e0
Ahmed <Ahmed@DESKTOP-UI6C79P.box>
2022-12-01 13:17:28 +0200

    draft commit

---
+++ src/main/java/me/edu/springadmin/springadmin/SpringAdminApplication.java
@@ -0,0 +1,13 @@
+package me.edu.springadmin.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
